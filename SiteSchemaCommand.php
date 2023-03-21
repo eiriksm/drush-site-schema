f9cffc4e9692d951c89388ca702422bc0c982cb8
Eirik Stanghelle Morland <eirik@morland.no>
2023-03-21 14:41:12 +0100

    Make it possible to disable modules from being counted (#10)

--- SiteSchemaCommand.php
+++ SiteSchemaCommand.php
@@ -3,6 +3,7 @@
 namespace Drush\Commands\site_schema;
 
 use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drupal\Core\Site\Settings;
 use Drupal\Core\Update\UpdateRegistry;
 use Drush\Commands\DrushCommands;
 
@@ -34,10 +35,34 @@ class SiteSchemaCommand extends DrushCommands {
     $method->invoke($registry);
     $method = $reflected->getMethod('getAvailableUpdateFunctions');
     $method->setAccessible(TRUE);
+    try {
+      $enabled_prop = $reflected->getProperty('enabledExtensions');
+    }
+    catch (\Throwable $e) {
+      // On Drupal 8 it's called this.
+      $enabled_prop = $reflected->getProperty('enabledModules');
+    }
+    $enabled_prop->setAccessible(TRUE);
+    $current = $enabled_prop->getValue($registry);
+    $disabled_modules = Settings::get('drush_site_schema_disabled_modules', []);
+    \Drupal::moduleHandler()->alter('drush_site_schema_disabled_modules', $disabled_modules);
+    $enabled_prop->setValue($registry, array_diff($current, $disabled_modules));
     $post_update_functions = $method->invoke($registry);
-    $modules = drupal_get_installed_schema_version(NULL, FALSE, TRUE);
+    try {
+      // This service was introduced in drupal 9.3.0.
+      /** @var \Drupal\Core\Update\UpdateHookRegistry $service */
+      $service = \Drupal::service('update.update_hook_registry');
+      $modules = $service->getAllInstalledVersions();
+    }
+    catch (\Throwable $e) {
+      // Maybe it's a pre-9.3.0 drupal.
+      $modules = drupal_get_installed_schema_version(NULL, FALSE, TRUE);
+    }
     // Now build the table.
     foreach ($modules as $module => $schema) {
+      if (in_array($module, $disabled_modules)) {
+        continue;
+      }
       $rows[] = [
         'type' => 'schema',
         'module' => $module,
